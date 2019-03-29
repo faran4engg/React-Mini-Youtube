7453e93ad434d9d3d2ae1f9f19a9544ebdab6fdf
Faran Shaikh <faran4engg@gmail.com>
2019-03-30 02:36:51 +0530

    upto receiving vdo data from YT API

---
+++ src/components/video/VideoListWrapper.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+class VideoListWrapper extends React.Component {
+  state = {};
+  render() {
+    return "VideoListWrapper";
+  }
+}
+
+export default VideoListWrapper;
