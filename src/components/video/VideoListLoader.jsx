ff3cfee01a9357ea941c8da6715495a83debe1cd
Faran Shaikh <faran4engg@gmail.com>
2019-03-30 21:08:21 +0530

    placeholder loader VideoListLoader.jsx

--- src/components/video/VideoListLoader.jsx
+++ src/components/video/VideoListLoader.jsx
@@ -2,14 +2,40 @@ import React from "react";
 
 const VideoListLoader = () => {
   return (
-    <div className="ui inverted segment">
-      <div className="ui active inverted placeholder">
-        <div className="image header">
-          <div className="line" />
-          <div className="line" />
+    <React.Fragment>
+      <div className="ui inverted segment">
+        <div className="ui active inverted placeholder">
+          <div className="image header">
+            <div className="line" />
+            <div className="line" />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="ui inverted segment">
+        <div className="ui active inverted placeholder">
+          <div className="image header">
+            <div className="line" />
+            <div className="line" />
+          </div>
+        </div>
+      </div>
+      <div className="ui inverted segment">
+        <div className="ui active inverted placeholder">
+          <div className="image header">
+            <div className="line" />
+            <div className="line" />
+          </div>
+        </div>
+      </div>
+      <div className="ui inverted segment">
+        <div className="ui active inverted placeholder">
+          <div className="image header">
+            <div className="line" />
+            <div className="line" />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
