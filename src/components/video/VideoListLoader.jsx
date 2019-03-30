fc4c5a8505665b4a26d0047eb85ec48769b2d5ba
Faran Shaikh <faran4engg@gmail.com>
2019-03-30 12:40:46 +0530

    video/VideoListLoader in Action

---
+++ src/components/video/VideoListLoader.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const VideoListLoader = () => {
+  return (
+    <div className="ui inverted segment">
+      <div className="ui active inverted placeholder">
+        <div className="image header">
+          <div className="line" />
+          <div className="line" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VideoListLoader;
