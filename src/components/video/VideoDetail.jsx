7d221ab92e3d9ada70e0ae6a4b402d17a722d4f4
Faran Shaikh <faran4engg@gmail.com>
2019-03-30 11:55:22 +0530

    VideoDetail component updates

--- src/components/video/VideoDetail.jsx
+++ src/components/video/VideoDetail.jsx
@@ -1,10 +1,25 @@
 import React from "react";
 
-class VideoDetails extends React.Component {
-  state = {};
-  render() {
-    return "VideoDetails";
-  }
-}
+const VideoDetails = props => {
+  const { displayVideo } = props;
 
+  if (!displayVideo) {
+    return "Loading...";
+  }
+  const videoSrcbaseUrl =
+    "https://www.youtube.com/embed/" + displayVideo.id.videoId;
+
+  return (
+    <React.Fragment>
+      <div className="ui embed">
+        <iframe title={displayVideo.snippet.title} src={videoSrcbaseUrl} />
+      </div>
+
+      <div className="ui segment">
+        <h4 className="ui header">{displayVideo.snippet.title}</h4>
+        <p>{displayVideo.snippet.description}</p>
+      </div>
+    </React.Fragment>
+  );
+};
 export default VideoDetails;
