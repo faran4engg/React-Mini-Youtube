a32adc8268453aeed831bb54c8d88598e8bf4524
Faran Shaikh <faran4engg@gmail.com>
2019-03-30 11:55:50 +0530

    VideoListWrapper component changes

--- src/components/video/VideoListWrapper.jsx
+++ src/components/video/VideoListWrapper.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import VideoItem from "./VideoItem";
 
-class VideoListWrapper extends React.Component {
-  state = {};
-  render() {
-    return "VideoListWrapper";
-  }
-}
+// class VideoListWrapper extends React.Component {
+//   state = {};
+//   render() {
+
+//     return "VideoListWrapper";
+//   }
+// }
+
+const VideoListWrapper = props => {
+  const { videoData, onVideoSelect } = props;
+  const renderedVideoItems = videoData.map(video => {
+    return (
+      <div key={video.etag} className="ui inverted segment">
+        <div className="ui inverted relaxed divided list">
+          <VideoItem onVideoSelect={onVideoSelect} videoItem={video} />
+        </div>
+      </div>
+    );
+  });
+
+  return <div>{renderedVideoItems}</div>;
+};
 
 export default VideoListWrapper;
+
+/*
+
+
+*/
