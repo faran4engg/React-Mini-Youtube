dc20d4fde0c0769795324f9b2050eca370b9146d
Faran Shaikh <faran4engg@gmail.com>
2019-03-30 11:55:35 +0530

    VideoItem updates

--- src/components/video/VideoItem.jsx
+++ src/components/video/VideoItem.jsx
@@ -1,10 +1,20 @@
+import "./VideoItem.css";
 import React from "react";
 
-class VideoItem extends React.Component {
-  state = {};
-  render() {
-    return "VideoItem";
-  }
-}
+const VideoItem = props => {
+  const { videoItem, onVideoSelect } = props;
+  return (
+    <div className="video-item item" onClick={() => onVideoSelect(videoItem)}>
+      <img
+        className="ui image"
+        src={videoItem.snippet.thumbnails.medium.url}
+        alt={videoItem.snippet.title}
+      />
+      <div className="content">
+        <div className="header">{videoItem.snippet.title}</div>
+      </div>
+    </div>
+  );
+};
 
 export default VideoItem;
